61acc8914cab5d7a1ba36dbe4d37c19ea9ed0878
badhri85 <badhri85@c220g2-010805.wisc.cloudlab.us>
2021-11-18 20:21:28 -0600

    send val instead of bitrate in case of bps

--- configapi/api_slice_mgmt.go
+++ configapi/api_slice_mgmt.go
@@ -53,7 +53,7 @@ func DeviceGroupDeleteHandler(c *gin.Context) bool {
 
 func convertToBps(val int64, unit string) (bitrate int64) {
 	if strings.EqualFold(unit, "bps") {
-		return bitrate
+		bitrate = val
 	} else if strings.EqualFold(unit, "kbps") {
 		bitrate = val * KPS
 	} else if strings.EqualFold(unit, "mbps") {

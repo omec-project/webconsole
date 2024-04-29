a9471dc1b39da332d3c57cd4ca33fa7cc02632db
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-04-29 12:30:37 -0400

    feat: expose metrics for prometheus to scrape (#164)

---
+++ backend/metrics/telemetry.go
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Canonical Ltd.
+
+/*
+ *  Metrics package is used to expose the metrics of the Webconsole service.
+ */
+
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/omec-project/webconsole/backend/logger"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// InitMetrics initializes Webconsole metrics
+func InitMetrics() {
+	http.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logger.InitLog.Errorf("Could not open metrics port: %v", err)
+	}
+}

357082e17762cfa148753748b76f63948df21f7c
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-06-25 10:41:07 +0000

    add example of ui static files
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

---
+++ ui/example/_next/static/chunks/app/layout-c93c367ddaaa2af8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6663:function(n,a,e){Promise.resolve().then(e.t.bind(e,3247,23))},3247:function(n){n.exports={style:{fontFamily:"'__Inter_aaf875', '__Inter_Fallback_aaf875'",fontStyle:"normal"},className:"__className_aaf875"}}},function(n){n.O(0,[972,971,23,744],function(){return n(n.s=6663)}),_N_E=n.O()}]);
